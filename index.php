0be4522bde1299bfe6dec2bfef176488a011bce4
somratcste <somratcste@gmail.com>
2017-01-06 23:11:04 +0600

    start Username Availiability Project

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php include('src/connection.php') ; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Ajax Jquery Project</title>
+	<link rel="stylesheet" type="text/css" href="src/css/style.css">
+</head>
+<body>
+	<div class="container">
+	<div class="row">	
+	<div class="col-md-6">	
+		<ul>
+			<li><a href="user.php" target="_blank">1. Ajax - Check Username Availiability</a></li>
+		</ul> 
+	</div>
+	</div>
+	</div>
+
+<script type="text/javascript" src="src/js/bootstrap.js"></script>
+<script type="text/javascript" src="src/js/jquery-3.1.0.min.js"></script>
+<script type="text/javascript" src="src/js/main.js"></script>
+</body>
+</html>
+
---
+++ user.php
@@ -0,0 +1,29 @@
+<?php include('src/connection.php') ; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Check Username Availiability</title>
+	<link rel="stylesheet" type="text/css" href="src/css/style.css">
+</head>
+<body>
+	<div class="container">
+	<div class="row">	
+	<div class="col-md-6">	
+		<form action="" method="post">
+			<tr>
+				<td>User Name : </td>
+				<td><input type="" name="username" id="username"></td>
+			</tr>
+		</form>
+	</div>
+	</div>
+	</div>
+
+<script type="text/javascript" src="src/js/bootstrap.js"></script>
+<script type="text/javascript" src="src/js/jquery-3.1.0.min.js"></script>
+<script type="text/javascript" src="src/js/main.js"></script>
+</body>
+</html>
+
