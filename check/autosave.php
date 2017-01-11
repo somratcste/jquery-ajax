71dcc2a221880713d07616517bd0df39ddc2317d
somratcste <somratcste@gmail.com>
2017-01-12 00:56:11 +0600

    Ajax auto save done

--- check/autosave.php
+++ check/autosave.php
@@ -6,10 +6,16 @@
 
 		if($contentID != ''){
 			$statement = $db->prepare("UPDATE autosave SET content=? WHERE contentID=?");
-			$statement->execute(array($contentID));
+			$statement->execute(array($content , $contentID));
 		} else {
 			$statement = $db->prepare("INSERT INTO autosave (content , status) VALUES (?,?)");
        		$statement->execute(array($content,"draft"));
+       		$statement = $db->prepare("SELECT `auto_increment` FROM INFORMATION_SCHEMA.TABLES WHERE table_name = 'autosave'");
+			$statement->execute(array());
+			$lastID = $statement->fetch()["auto_increment"];
+			$lastID = $lastID - 1 ;
+			echo $lastID ; 
+			exit() ;
 		}
 		
 	}
